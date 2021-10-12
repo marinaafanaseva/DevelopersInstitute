b0eaccdfca722af399979208f24e2940f4e510be
Maryna Afanasieva <91878721+marinaafanaseva@users.noreply.github.com>
2021-10-12 23:24:20 +0300

    Add files via upload

---
+++ Week2 Day1.js
@@ -0,0 +1,16 @@
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"]; //task definition;
+
+fruits.shift ();     //deleate start element of array;
+console.log (fruits);
+
+fruits.sort ();      //sort in alphabet;
+console.log (fruits);
+
+fruits.push ("Kiwi"); //add end element of array;
+console.log (fruits);
+
+fruits.splice (0,1);  //deleate first element;
+console.log (fruits);
+
+fruits.reverse ();    //sort in reverce order;
+console.log (fruits);
