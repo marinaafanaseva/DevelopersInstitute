4d951e7d0c78c30ceee238f55be7116bf18de53a
Maryna Afanasieva <91878721+marinaafanaseva@users.noreply.github.com>
2021-11-17 20:29:22 +0200

    Add files via upload

--- Week7_Day3.js
+++ Week7_Day3.js
@@ -1,11 +1,29 @@
-let fs = require('fs');
-fs.readFile('https://github.com/devtlv/Right-Left_Week7JS/blob/main/RightLeft.txt', 'utf-8', function (err, data) {
-    if (err) {
-        console.log(err)        
-    }
-    console.log(data);
-});
+const fs = require('fs');
 
-let x = String(data);
-let length = x.length;
-console.log(length); 
+fs.readFile('https://github.com/devtlv/Right-Left_Week7JS/blob/main/RightLeft.txt', 'utf-8', (err, data)=> {
+	if (err) {
+		console.log(err)
+	} else {
+		const data = data.toString()
+		let position = 0
+		let steps = 0
+		let first = 0
+		for (let i = 0; i < game.length; i++) {
+			steps++
+			if (game[i] === `>`) {
+				position++
+			} else {
+				position--
+			}
+			if (position === -1 && first === 0) {
+				console.log(`first time in left side is in ${steps} steps`)
+				first = 1
+			}
+		}
+		console.log(`total stepd: ${position}`)
+	}
+})
+
+//let x = String(data);
+//let length = x.length;
+//console.log(length);
